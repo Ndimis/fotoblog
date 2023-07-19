f8419bcedc9e28ae0e00ec71bf2b67b0b21dd5f5
Thierry Ndimis <tndimis@live.fr>
2023-07-18 23:10:02 -0400

    ajout du validateur personnalisé

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from . import validators
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=63, label='Nom d\'utilisateur')
@@ -10,3 +11,6 @@ class SignupForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ['username', 'email', 'first_name', 'last_name', 'role']
+
+# class PostCodeForm(forms.Form):
+#     post_code = forms.CharField(max_length=10, validators=[validators.PostCodeValidator])
---
+++ authentication/validators.py
@@ -0,0 +1,12 @@
+from django.core.exceptions import ValidationError
+from . import validators
+from django import forms
+
+class ContainsLetterValidator:
+    def validate(self, password, user=None):
+        if not any(char.is_alpha() for char in password):
+            raise ValidationError('Le mot de passe doit contenir une lettre', code='password_no_letters')
+
+    def get_help_text(self):
+        return 'Le mot de passe doit contenir au moins une lettre majuscule ou minuscule'
+    
